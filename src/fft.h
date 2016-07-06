5a8b3e2f3f3c1d77035624cc142b0373141f26e4
Tomasz Grel <you@example.com>
2016-07-06 14:33:00 +0200

    updated CMakeLists and minor changes in fft

--- src/fft.h
+++ src/fft.h
@@ -1,6 +1,10 @@
+#ifndef FFT_H
+#define FFT_H
+
 #include <vector>
 
 std::vector<double> fft(std::vector<double>& data, int begin=0, int end=-1);
 
 std::vector<std::vector<double>> spectrogram(std::vector<double> signal, int window);
 
+#endif
