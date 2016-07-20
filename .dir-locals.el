538802d2924fabd8df9a0bcf68f459b0f5ea5819
Michał Adamczyk <michal.anks@gmail.com>
2016-07-20 18:28:30 +0200

    emacs dirlocals update

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,2 +1,4 @@
-((nil . ((c-c++-default-mode-for-headers . 'c++-mode)
-         (cmake-ide-build-dir . "/home/anks/Sources/Projects/neuroon-alg-core/build"))))
+((nil . ((eval . (setq c-c++-default-mode-for-headers 'c++-mode))
+         (eval . (setq cmake-ide-build-dir (expand-file-name "~/Sources/Projects/neuroon-alg-core/build")))))
+ (c++-mode . ((flycheck-gcc-language-standard . "c++11")
+              (flycheck-clang-language-standard . "c++11"))))
