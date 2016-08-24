2b62d4a23449e2815274ea2916bf6a22674829b9
Tomasz Grel <you@example.com>
2016-08-24 12:05:17 +0200

    minor style correction

--- test/logger_test.h
+++ test/logger_test.h
@@ -1,12 +1,5 @@
-//#include <easylogging/easylogging.h>
-
-//#define ELPP_DISABLE_LOGS
-//#include "easylogging++.h"
-#include <iostream>
-#include <fstream>
-
+#include <gtest/gtest.h>
 #include "logger.h"
-
 
 TEST(LoggerTest, basic_log_test) {
 	LOG(INFO) << "LOGGING TEST!";
