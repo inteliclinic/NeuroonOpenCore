acc11120537814438c2241380c4ae841f90c20c8
Tomasz Grel <you@example.com>
2016-08-25 09:28:17 +0200

    Further changes to the logger

--- src/logger.h
+++ src/logger.h
@@ -47,7 +47,12 @@ template<typename T=DummyCallback>
 void configure_logger() {
 	el::Helpers::installLogDispatchCallback<T>("default");
 	el::Loggers::addFlag(el::LoggingFlag::ColoredTerminalOutput);
-	el::Loggers::reconfigureAllLoggers(el::ConfigurationType::Format, "%datetime %level %logger | %func@%loc\n%msg");
+
+	// el::Loggers::reconfigureAllLoggers(el::ConfigurationType::Format, "%datetime %level %logger %fbase:%line  %msg");
+	el::Loggers::reconfigureAllLoggers(el::ConfigurationType::Format, "%datetime{%d/%M %h:%m:%s,%g} %level [%logger] %fbase:%line: %msg");
+  // michal debug
+	// el::Loggers::reconfigureAllLoggers(el::ConfigurationType::Format, "%datetime{%d/%M %h:%m:%s,%g} %level %logger | %func@%fbase:%line\n%msg");
+
 	CLOG(INFO, "default") << "Logger initialized";
 }
 
