ccac63d65611563bb3cd2742bb909ef8ff08ce15
Tomasz Grel <you@example.com>
2016-08-26 14:38:39 +0200

    First working version of online sleep staging algorithm. Still not perfect

---
+++ test/online_staging_functional_test.h
@@ -0,0 +1,36 @@
+#include <gtest/gtest.h>
+#include <dlib/matrix.h>
+#include <iostream>
+#include "OfflineStagingClassifier.h"
+#include "StagingPreprocessor.h"
+
+#include "functional_tests_data.h"
+
+TEST(StagingFunctionalTest, staging_functional_test) {
+	dlib::matrix<double> features = get_python_features();
+	OfflineStagingClassifier* clf = OfflineStagingClassifier::get_instance();
+	dlib::matrix<int> stages = clf->predict(features);
+
+	std::ofstream out("./functional_test_results/staging.csv");
+	if(!out.good()) {
+		throw std::logic_error("Could not open the file");
+	}
+	out << dlib::csv << stages;
+}
+
+TEST(StagingFunctionalTest, full_offline_staging_functional_test) {
+	dlib::matrix<double> eeg = get_eeg_data();
+	dlib::matrix<double> ir = get_ir_data();
+
+	StagingPreprocessor pre;
+	dlib::matrix<double> features = pre.transform(eeg, ir);
+
+	OfflineStagingClassifier* clf = OfflineStagingClassifier::get_instance();
+	dlib::matrix<int> stages = clf->predict(features);
+
+	std::ofstream out("./functional_test_results/full_offline_staging.csv");
+	if(!out.good()) {
+		throw std::logic_error("Could not open the file");
+	}
+	out << dlib::csv << stages;
+}
