fa39f9c4fc9ccb958110a82fbacc49013678d225
Tomasz Grel <you@example.com>
2016-09-26 10:46:08 +0200

    initial brain-wave levels; not yet working

---
+++ src/sleep_staging/online/BrainWaveLevels.cpp
@@ -0,0 +1,18 @@
+/*
+ * BrainWaveLevels.cpp
+ *
+ *  Created on: Sep 26, 2016
+ *      Author: tomek
+ */
+
+#include "BrainWaveLevels.h"
+
+BrainWaveLevels::BrainWaveLevels() {
+	// TODO Auto-generated constructor stub
+
+}
+
+BrainWaveLevels::~BrainWaveLevels() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/sleep_staging/online/BrainWaveLevels.h
@@ -0,0 +1,17 @@
+/*
+ * BrainWaveLevels.h
+ *
+ *  Created on: Sep 26, 2016
+ *      Author: tomek
+ */
+
+#ifndef SRC_SLEEP_STAGING_ONLINE_BRAINWAVELEVELS_H_
+#define SRC_SLEEP_STAGING_ONLINE_BRAINWAVELEVELS_H_
+
+class BrainWaveLevels {
+public:
+	BrainWaveLevels();
+	virtual ~BrainWaveLevels();
+};
+
+#endif /* SRC_SLEEP_STAGING_ONLINE_BRAINWAVELEVELS_H_ */
