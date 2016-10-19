13e7e0dc0675f4a4653dae86526e51bdf4dbceb4
Pawel Kazmierzewski <p.kazmierzewski@inteliclinic.com>
2016-10-19 14:35:30 +0200

    Add low level api

---
+++ api/NeuroonLowMaskApi.h
@@ -0,0 +1,15 @@
+/**
+ * @file    NeuroonLowMaskApi.h
+ * @Author  Paweł Kaźmierzewski <p.kazmierzewski@inteliclinic.com>
+ * @date    October, 2016
+ * @brief   Brief description
+ *
+ * Description
+ */
+
+#ifndef NEUROONLOWMASKAPI_H
+#define NEUROONLOWMASKAPI_H
+
+#include "ic_low_level_control.h
+
+#endif /* !NEUROONLOWMASKAPI_H */
