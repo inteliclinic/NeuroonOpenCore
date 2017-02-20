1a7073d7862c4102512ec706e3e0c581b998ae76
Pawel Kazmierzewski <p.kazmierzewski@inteliclinic.com>
2017-02-20 17:05:34 +0100

    Add dfu API definition

--- api/NeuroonDfuApi.h
+++ api/NeuroonDfuApi.h
@@ -10,8 +10,17 @@
 #ifndef NEUROONMASKCOMMAPI_H
 #define NEUROONMASKCOMMAPI_H
 
+#ifdef __cplusplus
+#include <climits> // CHAR_BIT macro
+#include <cstddef> // size_t type
+#include <cstdint>
+extern "C" {
+#else
 #include <limits.h> // CHAR_BIT macro
 #include <stddef.h> // size_t type
+#include <stdbool.h>
+#include <stdint.h>
+#endif
 
 #if CHAR_BIT != 8
 #error "Char is not 8 bit!"
@@ -23,8 +32,8 @@
  * Encodes major releases.
  * */
 typedef enum{
-  LEGACY_NEUROON_FIRMWARE = 0x00, /**< firmware 2.0.x.x */
-  NEW_NEUROON_FIRMWARE = 0x01     /**< firmware 2.1.x.x */
+  LEGACY_NEUROON_FW = 0x00, /**< firmware 2.0.x.x */
+  NEW_NEUROON_FW = 0x01     /**< firmware 2.1.x.x */
 }ncFirmwareMilestone;
 
 /**
@@ -33,11 +42,20 @@ typedef enum{
  * Encoded orders for application to execute
  */
 typedef enum{
-  DFU_SEND_NEXT_DATASET = 0x00, /**< send data stored in output frame */
-  DFU_RESEND_DATASET,           /**< resend previous data set */
-  DFU_TERMINATE,                /**< Mask encoutered critical error in DFU mode */
-  DFU_END                       /**< Update finished */
+  DFU_SEND_NEXT = 0x00, /**< send data stored in output frame */
+  DFU_RESEND,           /**< resend previous data set */
+  DFU_TERM,             /**< Mask encoutered critical error in DFU mode */
+  DFU_FINISHED          /**< Update finished */
 }ncDfuAction;
+
+/**
+ * @brief Firmware type
+ */
+typedef enum{
+  SD_FIRM   = 0x00,  /**< Nordic Soft Device */
+  APP_FIRM  = 0x01,  /**< Neuroon firmware */
+  DFU_FIRM  = 0x02   /**< DFU */
+}ncFirmwareType;
 
 /**
  * @brief Generate "go to dfu" command
@@ -56,12 +74,12 @@ bool ncGotoDfu(char *frame, size_t *len, ncFirmwareMilestone firmware);
 /**
  * @brief Receive data from rdfu response characteristic
  *
- * @param[in]   responseFrame  20 byte array with response frame
- * @param[in]   responseLen    response array length
- * @param[out]  frame           pointer to 20 bytes array where binary data will
- * be stored
- * @param[out]  len             pointer to size_t value where function will pit
- * output frame length
+ * @param[in]   responseFrame 20 byte array with response frame
+ * @param[in]   responseLen   response array length
+ * @param[out]  frame         pointer to 20 bytes array where binary data will
+ *                            be stored
+ * @param[out]  len           pointer to size_t value where function will pit
+ *                            output frame length
  *
  * @return returns next step for update
  */
@@ -70,13 +88,20 @@ ncDfuAction ncDfuResponseSink(char *responseFrame, size_t responseLen, char* fra
 /**
  * @brief Generate start update command for dfu
  *
- * @param[out]  frame pointer to 20 bytes array where frame will be stored
- * @param[out]  len   pointer to size_t value where function will put lenght of
- * array
+ * @param[out]  frame     pointer to 20 bytes array where frame will be stored
+ * @param[out]  len       pointer to size_t value where function will put lenght of array
+ * @param[in]   fb        pointer to buffer with binary file
+ * @param[in]   file_len  binary file length
+ * @param[in]   firm      type of firmware @ref ncFirmwareType
+ * @param[in]   version   binary version build from 4 bytes. Ex 16777985 = 1.0.3.1
  *
  * @return true if function was able to generate frame, false otherwise(ex.
  * wrong frame len)
  */
-bool ncDfuStartUpdate(char *frame, size_t *len);
+bool ncDfuStartUpdate(char *frame, size_t *len, char *fb, size_t file_len, ncFirmwareType firm,
+    uint32_t version);
 
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
 #endif /* !NEUROONMASKCOMMAPI_H */
---
+++ src/NeuroonDFU.cpp
@@ -0,0 +1,57 @@
+/**
+ * @file    NeuroonDfu.cpp
+ * @Author  Paweł Kaźmierzewski <p.kazmierzewski@inteliclinic.com>
+ * @date    February, 2017
+ * @brief   Brief description
+ *
+ * Description
+ */
+
+#include "NeuroonDfuApi.h"
+#include "../external_modules/unified_communication/API/ic_dfu.h"
+#include <iostream>
+
+#define PRINT_DBG_INFO std::cout<<"running: "<<__FILE__<<"::"<<__func__<<std::endl
+
+bool ncGotoDfu(char *frame, size_t *len, ncFirmwareMilestone firmware){
+  PRINT_DBG_INFO;
+  e_firmwareMilestone _firmware = firmware == NEW_NEUROON_FW ? NEW_NEUROON_FIRMWARE :
+    LEGACY_NEUROON_FIRMWARE;
+
+  goto_dfu(frame, len, _firmware);
+
+  return true;
+}
+
+ncDfuAction ncDfuResponseSink(char *responseFrame, size_t responseLen, char* frame, size_t *len){
+  PRINT_DBG_INFO;
+  switch(dfu_response_sink(responseFrame, responseLen, frame, len)){
+    case DFU_SEND_NEXT_DATASET:
+      return DFU_SEND_NEXT;
+    case DFU_RESEND_DATASET:
+      return DFU_RESEND;
+    case DFU_TERMINATE:
+      return DFU_TERM;
+    case DFU_END:
+      return DFU_FINISHED;
+  }
+}
+
+bool ncDfuStartUpdate(char *frame, size_t *len, char *fb, size_t file_len, ncFirmwareType firm,
+    uint32_t version){
+  PRINT_DBG_INFO;
+  e_firmwareType _firm;
+  switch(firm){
+    case SD_FIRM:
+      _firm = SD_FIRMWARE;
+      break;
+    case APP_FIRM:
+      _firm = APP_FIRMWARE;
+      break;
+    case DFU_FIRM:
+      _firm = DFU_FIRMWARE;
+      break;
+  }
+  dfu_start_update(frame, len, fb, file_len, _firm, version);
+  return true;
+}
