1f7c7d575f792e39790ee5c5b4bad02734a5d094
Pawel Kazmierzewski <p.kazmierzewski@inteliclinic.com>
2017-03-27 22:42:15 +0200

    add characteristics getters

--- src/NeuroonHAL.cpp
+++ src/NeuroonHAL.cpp
@@ -16,3 +16,40 @@
 void ncNeuroonHalInit(ncCharacteristicsNames chars){
   nuc_init(chars);
 }
+
+int ncGetEEGCharacteristicIndex(){
+  return DATA_STREAM0_UUID;
+}
+
+int ncGetPATCharacteristicIndex(){
+  return DATA_STREAM1_UUID;
+}
+
+int ncGetStatusCharacteristicIndex(){
+  return STATUS_STREAM_UUID;
+}
+
+int ncGetResponseCharacteristicIndex(){
+  return RESPONSE_UUID;
+}
+
+int ncGetCMDCharacteristicIndex(){
+  return CMD_UUID;
+}
+
+int ncGetDFUSettingsTxCharacteristicIndex(){
+  return SETTINGS_TX_UUID;
+}
+
+int ncGetDFUTxCharacteristicIndex(){
+  return DFU_TX_UUID;
+}
+
+int ncGetDFUSettingsRxCharacteristicIndex(){
+  return SETTINGS_RX_UUID;
+}
+
+int ncGetDFURxCharacteristicIndex(){
+  return DFU_RX_UUID;
+}
+
