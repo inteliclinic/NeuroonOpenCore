f23daeb14eb8a9d6a0096d36c4a3697b0cbaad01
Michał Adamczyk <michal.anks@gmail.com>
2017-04-24 01:22:39 +0200

    Add electrodes contact test micro scenario (unfinished).

---
+++ src/scenarios/microscenarios/ContactTestScenario.cpp
@@ -0,0 +1,101 @@
+#include "ContactTestScenario.h"
+#include <algorithm>
+
+const std::vector<ncAtomicInstruction> &
+ContactTestScenario::_getLightsOnInstructions() {
+  if (this->_lights_on_instructions.empty()) {
+    // [TODO] prepare actual instructions and set it
+  }
+  return this->_lights_on_instructions;
+}
+
+const std::vector<ncAtomicInstruction> &
+ContactTestScenario::_getLightsOffInstructions() {
+  if (this->_lights_off_instructions.empty()) {
+    // [TODO] prepare actual instructions and set it
+  }
+  return this->_lights_off_instructions;
+}
+
+ncUpdateOutput ContactTestScenario::go(const std::set<Key> &triggers) {
+
+  // after timeout scenario is not to be used
+  if (triggers.find(kDontWakeTimer) != triggers.end()) {
+    return this->finish();
+  }
+  // if signal is lost
+  if (triggers.find(kSignalLost) != triggers.end()) {
+
+    // lights are already on and not planned to turn off
+    if (this->_lights_would_turn_off_at == 0 &&
+        // this should be true, unlees on start
+        this->_lights_would_turn_on_at != 0) {
+      return UPDATE_OK;
+    }
+    // lights are already on but are planned to be turn off
+    else if (this->_lights_would_turn_off_at > this->currentMoment()) {
+      // in this case cancel turning off
+      this->clearInstructions();
+      return UPDATE_NEW_DATA;
+    } else {
+      // we can assume lights are already off
+      this->_lights_would_turn_off_at = 0;
+      // lights are scheduled on
+      if (this->_lights_would_turn_on_at > this->currentMoment()) {
+        return UPDATE_OK;
+      } else {
+        // lights should be scheduled
+        auto inss = this->_getLightsOnInstructions();
+        this->pushInstructions(inss);
+        this->_lights_would_turn_on_at =
+            this->currentMoment() + 1 + inss[0].time;
+        return UPDATE_NEW_DATA;
+      }
+    }
+  }
+  // signal is not lost
+  else {
+    // lights are already off and not planned to turn on
+    if (this->_lights_would_turn_on_at == 0
+        // this should be true
+        && this->_lights_would_turn_off_at != 0) {
+      return UPDATE_OK;
+    }
+    // lights are already off but are planned to be turn on
+    else if (this->_lights_would_turn_on_at > this->currentMoment()) {
+      // in this case cancel turning on
+      this->clearInstructions();
+      return UPDATE_NEW_DATA;
+    } else {
+      // we can assume lights are already on
+      this->_lights_would_turn_on_at = 0;
+      // lights are scheduled off
+      if (this->_lights_would_turn_off_at > this->currentMoment()) {
+        return UPDATE_OK;
+      } else {
+        // lights off should be scheduled
+        auto inss = this->_getLightsOffInstructions();
+        this->pushInstructions(inss);
+        this->_lights_would_turn_off_at =
+            this->currentMoment() + 1 + inss[0].time;
+        return UPDATE_NEW_DATA;
+      }
+    }
+  }
+}
+
+ncUpdateOutput ContactTestScenario::finish() {
+
+  // turn off the lights if they are turned on, or cancel if planned ot be
+  // turned on
+  // [TODO]
+  return UPDATE_OK;
+}
+
+ncUpdateOutput ContactTestScenario::mute() {
+
+  // turn off the lights if they are turned on, or cancel if planned ot be
+  // turned on
+  // [TODO]
+  return UPDATE_OK;
+}
---
+++ src/scenarios/microscenarios/ContactTestScenario.h
@@ -0,0 +1,58 @@
+/**
+ * @file    ContactTestScenario.h
+ * @Author  Michał Adamczyk <m.adamczyk@inteliclinic.com>
+ * @date    April, 2017
+ * @brief   Brief description
+ *
+ * Micro scenario used for warning user about bad signal
+ * or lost electrode contact
+ */
+
+#ifndef CONTACTTESTSCENARIO_H
+#define CONTACTTESTSCENARIO_H
+
+#include "IScenarioTrigger.h"
+#include "MicroScenario.h"
+#include "NeuroonApiCommons.h"
+#include <vector>
+
+
+class ContactTestScenario : public MicroScenario {
+
+private:
+  const int kSignalLost = 1;
+  const int kDontWakeTimer = 2;
+
+  ncUnixTimestamp _lights_would_turn_on_at = 0;
+  ncUnixTimestamp _lights_would_turn_off_at = 0;
+
+  int _r, _g, _b;
+  float _intensity;
+
+  std::vector<ncAtomicInstruction> _lights_on_instructions = {};
+  std::vector<ncAtomicInstruction> _lights_off_instructions {};
+
+  const std::vector<ncAtomicInstruction> & _getLightsOnInstructions();
+  const std::vector<ncAtomicInstruction> & _getLightsOffInstructions();
+
+public:
+
+  ContactTestScenario() : ContactTestScenario(0xff,0x80,0x00, 0.9) {}
+
+  ContactTestScenario(int r, int g, int b, float intensity=1.0) :
+    _r(r), _g(g), _b(b), _intensity(intensity) {}
+
+  ncUpdateOutput go(const std::set<Key> &triggers) override;
+  ncUpdateOutput finish() override;
+  ncUpdateOutput mute() override;
+
+  void installSignalLostTrigger(const IScenarioTrigger *t) {
+    this->installTriggerForKey(t, kSignalLost);
+  }
+
+  void installDontWakeTrigger(const IScenarioTrigger *t) {
+    this->installTriggerForKey(t, kDontWakeTimer);
+  }
+};
+
+#endif /* !CONTACTTESTSCENARIO_H */
