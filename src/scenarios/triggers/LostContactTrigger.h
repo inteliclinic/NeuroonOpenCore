722590d00de8a35330c6d336e1c4dfb05839ef37
Michał Adamczyk <michal.anks@gmail.com>
2017-04-24 01:22:21 +0200

    Add lost contact trigger implementation.

---
+++ src/scenarios/triggers/LostContactTrigger.h
@@ -0,0 +1,40 @@
+/**
+ * @file    LostContactTrigger.h
+ * @Author  Michał Adamczyk <m.adamczyk@inteliclinic.com>
+ * @date    April, 2017
+ * @brief   Brief description
+ *
+ * Simple trigger that will be active when the electrode contact is lost.
+ */
+
+#ifndef LOSTCONTACTTRIGGER_H
+#define LOSTCONTACTTRIGGER_H
+
+
+#include "NeuroonApiCommons.h"
+#include "ScenarioTrigger.h"
+
+typedef std::pair<ncUnixTimestamp, ncSignalQuality> lttInput;
+
+class LostContactTrigger : public ScenarioTrigger<lttInput> {
+
+  LostContactTrigger(int switch_treshold_ms,
+                     ncUnixTimestamp trigger_timeout_timestamp,
+                     ncSignalQuality lost_contact_treshold);
+  void update(const lttInput &inp) override;
+
+  bool isActive() const override;
+
+private:
+  ncUnixTimestamp _start = 0;
+  int _switch_treshold_ms = 0;
+  ncUnixTimestamp _trigger_timeout_timestamp = 0;
+
+  ncSignalQuality _lost_contact_treshold = NO_SIGNAL;
+  bool _last_contact_change_below_treshold = NO_SIGNAL;
+  ncUnixTimestamp _last_contact_change_ts = 0;
+
+  bool _active = false;
+};
+
+#endif /* !LOSTCONTACTTRIGGER_H */
