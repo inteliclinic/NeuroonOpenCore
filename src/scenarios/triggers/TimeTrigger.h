57b7706077d55ecb0380a1d9297a2b8541167ffc
Michał Adamczyk <michal.anks@gmail.com>
2017-04-30 13:30:40 +0200

    Move TimeTrigger creation to constructors.

--- src/scenarios/triggers/TimeTrigger.h
+++ src/scenarios/triggers/TimeTrigger.h
@@ -14,44 +14,28 @@
 #include "ScenarioTrigger.h"
 #include <functional>
 
-class TimeTrigger
-    : public StatefulTrigger<ncUnixTimestamp, ncUnixTimestamp> {
+enum class TimeTriggerMode { PASSED, ABSOLUTE };
 
-  enum class TimeMode{
-    PASSED,
-    ABSOLUTE
-  };
+class TimeTrigger : public StatefulTrigger<ncUnixTimestamp, ncUnixTimestamp> {
 
-  TimeTrigger(std::function<bool(int)> time_activation, TimeMode mode)
-    : StatefulTrigger(0, [time_activation, mode](ncUnixTimestamp &s,
-                                                 ncUnixTimestamp current_time) {
-                        if (s == 0)
-                          s = current_time;
-                        auto time_arg = mode == TimeMode::PASSED ? current_time - s : current_time;
-                        return time_activation(time_arg);
-                      }) {}
 public:
+  TimeTrigger(int start_time_ms, int end_time_ms, TimeTriggerMode  mode)
+      : TimeTrigger(
+            [start_time_ms, end_time_ms](int passed_ms) {
+              return passed_ms >= start_time_ms && passed_ms <= end_time_ms;
+            },
+            mode) {}
 
-  static TimeTrigger Passed(int start_time_ms, int end_time_ms ){
-    return TimeTrigger([start_time_ms, end_time_ms](int passed_ms) {
-        return passed_ms >= start_time_ms && passed_ms <= end_time_ms;
-      }, TimeMode::PASSED);
-  }
-
-  static TimeTrigger Passed(std::function<bool(int)> time_activation){
-    return TimeTrigger(time_activation, TimeMode::PASSED);
-  }
-
-  static TimeTrigger Absolute(ncUnixTimestamp start_time_ms, ncUnixTimestamp end_time_ms){
-    return TimeTrigger([start_time_ms, end_time_ms](int passed_ms) {
-        return passed_ms >= start_time_ms && passed_ms <= end_time_ms;
-      }, TimeMode::ABSOLUTE);
-  }
-
-  static TimeTrigger Absolute(std::function<bool(int)> time_activation){
-    return TimeTrigger(time_activation, TimeMode::ABSOLUTE);
-  }
-
+  TimeTrigger(std::function<bool(int)> time_activation, TimeTriggerMode  mode)
+      : StatefulTrigger(
+            0, [time_activation, mode](ncUnixTimestamp &s,
+                                       ncUnixTimestamp current_time) {
+              if (s == 0)
+                s = current_time;
+              auto time_arg =
+                  mode == TimeTriggerMode::PASSED ? current_time - s : current_time;
+              return time_activation(time_arg);
+            }) {}
 };
 
 #endif /* !TIMETRIGGER_H */
