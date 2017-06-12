b5088adc482466d21051f09014f6f50f13a3e354
Michał Adamczyk <michal.anks@gmail.com>
2017-06-12 18:03:14 +0200

    Add mocked napping schedule.

--- src/StandaloneAlgorithmsMock.cpp
+++ src/StandaloneAlgorithmsMock.cpp
@@ -1,19 +1,21 @@
 #import "NeuroonStandaloneAlgorithmsApi.h"
 
-ncDayTimeInstant ncEstimateCrossoverTime(ncDayTimeInstant usual_wake_up_time
-                                         ) {
+ncDayTimeInstant ncEstimateCrossoverTime(ncDayTimeInstant usual_wake_up_time) {
   // TODO prepare actual logic here
   auto &m = usual_wake_up_time.minute;
   auto &h = usual_wake_up_time.hour;
 
   m -= 20;
-  if (m<0) h--;
-  h += m/60;
+  if (m < 0)
+    h--;
+  h += m / 60;
   m = m % 60;
-  if(m<0) m+=60;
+  if (m < 0)
+    m += 60;
   h -= 2;
   h = h % 24;
-  if(h<0) h+= 24;
+  if (h < 0)
+    h += 24;
 
   return {h, m};
 }
@@ -21,17 +23,26 @@ ncDayTimeInstant ncEstimateCrossoverTime(ncDayTimeInstant usual_wake_up_time
 ncDayTimeInstant
 ncCalculateCrossoverPoint(ncDayTimeInstant current_crossover,
                           ncCircadianRhythmEvent *new_biorhythm_events,
-                          unsigned long events_count){
+                          unsigned long events_count) {
   // TODO prepare actual logic here
-  int m  = current_crossover.minute;
+  int m = current_crossover.minute;
   int h = current_crossover.hour;
 
   m += events_count;
-  h+=m/60;
+  h += m / 60;
   m = m % 60;
   h = h % 24;
 
   current_crossover.minute = m % 60;
   current_crossover.hour = h % 24;
   return current_crossover;
+}
+
+ncNappingSchedule
+compute_napping_schedule(ncDayTimeInstant current_crossover,
+                         ncCircadianRhythmEvent *recent_biorhythm_events,
+                         ncStagingElement **recent_nocturnal_sleeps) {
+
+  return {{14, 00}, {19, 00}, {16, 00}, {19, 00},
+          {12, 00}, {14, 00}, {15, 30}, {17, 30}};
 }
